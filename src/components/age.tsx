0349ad10f67a2f0367914fa14469c8da75495c9e
Josh Delgado <josh.high5@gmail.com>
2023-02-21 11:07:45 -0700

    Made age function less wild

--- src/components/age.tsx
+++ src/components/age.tsx
@@ -1,84 +1,64 @@
 
 const msInDay: number = 86400000,
-	daysInWeek: number = 7,
-	daysInMonth: number = 30,
-	daysInYear: number = 365;
-// monthsInYear: number = 12,
-// weeksInYear: number = 52,
-// weeksInMonth: number = 4,
-// displayOptions: string[] = ['day', 'week', 'month', 'yeah', 'detailed', 'second', 'minute', 'hour'];
+	appaBirthday: string = '10/22/2022',
+	appaGotchaday: string = '1/27/2023',
+	displayOptions: string[] = ['day', 'week', 'month', 'yeah', 'detailed', 'second', 'minute', 'hour'];
 
 export default function Age() {
 	const today: Date = new Date(),
-		birthday: Date = new Date('10/22/2022'),
-		ageInMs: any = today.getTime() - birthday.getTime();
-
-	let age = getReadableAge(ageInMs);
+		birthday: Date = new Date(appaBirthday),
+		age = getAgeInNaturalLanguage(today, birthday);
 
 	return (
 		<h2 className="appa"><span>Appa</span> is {age} old!</h2>
 	);
 }
 
-function getReadableAge(ageInMs: number): string {
-	const ageInDays: number = Math.floor(ageInMs / msInDay),
-		// ageInWeeks: number = Math.floor(ageInDays / daysInWeek),
-		// ageInMonths: number = Math.floor(ageInDays / daysInMonth),
-		ageInYears: number = Math.floor(ageInDays / daysInYear);
-
-	let ageObj: { [index: string]: number } = {
-		year: 0,
-		month: 0,
-		week: 0,
-		day: 0,
-	};
-	let age: string = '';
-
-	let remainingDays = ageInDays;
-	if (ageInYears >= 1) {
-		ageObj.year += ageInYears;
-		remainingDays -= (ageInYears * daysInYear);
-	} else {
-		delete ageObj.year;
-	}
-	if (remainingDays / daysInMonth >= 1) {
-		ageObj.month += Math.floor(remainingDays / daysInMonth);
-		remainingDays -= Math.floor(remainingDays / daysInMonth) * daysInMonth;
-	} else {
-		delete ageObj.month;
-	}
-
-	if (remainingDays / daysInWeek >= 1) {
-		ageObj.week += Math.floor(remainingDays / daysInWeek);
-		remainingDays -= Math.floor(remainingDays / daysInWeek) * daysInWeek;
-	} else {
-		delete ageObj.week;
-	}
-	if (remainingDays >= 1) {
-		ageObj.day += remainingDays;
-	} else {
-		delete ageObj.day;
-	}
-
-
-	const lastKey = Object.keys(ageObj).pop();
+function getAgeInNaturalLanguage(today: Date, birthday: Date): string {
+	const ageObj = getAgeObject(today, birthday),
+		lastKey = Object.keys(ageObj).pop();
+	let ageInNaturalLanguage = '';
 
 	for (const k in ageObj) {
 		const v = ageObj[k];
-		console.log(`${k}: ${v}`);
 
 		if (k === lastKey) {
-			age += 'and '
+			ageInNaturalLanguage += 'and '
 		}
 
 		if (v > 0) {
-			age += `${v} ${k}`;
+			ageInNaturalLanguage += `${v} ${k}`;
 			if (v > 1) {
-				age += 's';
+				ageInNaturalLanguage += 's';
 			}
-			age += ' ';
+			ageInNaturalLanguage += ' ';
 		}
 	}
 
-	return age;
+	return ageInNaturalLanguage;
+}
+
+function getAgeObject(today: Date, birthday: Date): { [index: string]: number } {
+	const timeBetween = today.getTime() - birthday.getTime()
+	let totalDays = Math.floor(timeBetween / msInDay),
+		totalWeeks = Math.floor(totalDays / 7),
+		totalMonths = Math.floor(totalDays / 30),
+		years = Math.floor(totalDays / 365),
+		remainingDays = totalDays %= 30,
+		remainingWeeks = 0,
+		remainingMonths = totalMonths %= 12;
+
+	if (remainingDays >= 7) {
+		remainingWeeks = Math.floor(remainingDays / 7);
+		remainingDays = remainingDays % 7;
+	}
+
+	const ageObj: { [index: string]: number } = {
+		year: years,
+		month: remainingMonths,
+		week: remainingWeeks,
+		day: remainingDays
+	};
+
+	return ageObj;
 }
